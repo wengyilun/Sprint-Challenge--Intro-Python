d48275fec435a4e421c1393cb87d056e1040f634
Yilun Weng <wengellen@gmail.com>
2020-02-22 10:53:52 -0800

    Finishes cityreader MVP

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,6 +1,17 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
 
+import csv
+
+class City:
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
+
+  def __str__(self):
+    return f'{self.name},{self.lat},{self.lon}'
+
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -15,13 +26,17 @@
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
 cities = []
-
 def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
-    return cities
+      with open('cities.csv', newline='') as csvfile:
+        data = csv.reader(csvfile, delimiter=',')
+        cityData = list(data)
+        for index in range(1, len(cityData)):
+          row = cityData[index]
+          cities.append(City(row[0], float(row[3]), float(row[4])))
+      return cities
 
 cityreader(cities)
 
@@ -29,6 +44,7 @@ cityreader(cities)
 for c in cities:
     print(c)
 
+ #City("Seattle", 47.6217,-122.3238),
 # STRETCH GOAL!
 #
 # Allow the user to input two points, each specified by latitude and longitude.
